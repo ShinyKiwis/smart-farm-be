02c029f39ad8f2ec197cb49413441a4e399e57d0
ShinyKiwis <nguyenhua.hcm@gmail.com>
2023-03-09 17:00:57 +0700

    Update: Implement User Model

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,11 +1,20 @@
 import mongoose from "mongoose"
+import UserModel from "../users/user.model"
+
+interface Models {
+  User: typeof UserModel;
+}
 
 class Database {
   private static _instance: Database;
+  private _models: Models;
 
   private constructor() {
     // Initialize mongo
     mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}${process.env.MONGO_ENDPOINT}`)
+    this._models = {
+      User: new UserModel()
+    }
   }
 
   static getInstance() {
