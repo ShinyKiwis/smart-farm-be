9d6c7e536f363966a4c7d5c6603913d5002d93fb
ShinyKiwis <nguyenhua.hcm@gmail.com>
2023-03-31 22:14:28 +0700

    Update: Change from using webhook to long polling

--- src/controllers/adafruit/adafruit.controller.ts
+++ src/controllers/adafruit/adafruit.controller.ts
@@ -8,6 +8,7 @@ class AdafruitController implements Controller {
     public path = '/adafruit';
     public router = express.Router();
     private observer = new Observer(Client.getInstance());
+    private feedKeys = ['temperature', 'humidity', 'moisture', 'light', 'gdd']
 
     constructor() {
         this.initializeRoute();
@@ -16,14 +17,26 @@ class AdafruitController implements Controller {
     private initializeRoute () {
         this.router.get(`${this.path}/:feedKey`, this.getFeedData);
         this.router.post(`${this.path}/:feedKey/:data`, this.updateFeedData);
-        this.router.post(`${this.path}/`, this.getDataFromAdafruit)
+        // this.router.post(`${this.path}/`, this.getDataFromAdafruit)
         this.router.get(`${this.path}/:feedKey/latest`, this.getLatestData);
+        this.feedKeys.forEach(feedKey => {
+          this.pollForData(feedKey)
+        })
     }
 
-    private getDataFromAdafruit = (request: express.Request, response: express.Response) => {
-      console.log(request.body[0])
-      this.observer.notify(JSON.stringify(request.body[0]))
-      // response.status(200).end()
+    private pollForData = (feedKey: string) => {
+      setInterval(async ()=>{
+        const {data} = await axios.get(`https://io.adafruit.com/api/v2/meodihere/feeds/${feedKey}/data/retain`, {
+            params: {
+                'x-aio-key': process.env.ADAFRUIT_APIKEY 
+            },
+        })
+        const result = {
+          [feedKey]: data.trim().replace(/,/g,"")
+        }
+        console.log(JSON.stringify(result))
+        this.observer.notify(JSON.stringify(result))
+      }, 32000)
     }
 
     private getFeedData = (request: express.Request, response: express.Response) => {
@@ -60,7 +73,9 @@ class AdafruitController implements Controller {
                 'x-aio-key': process.env.ADAFRUIT_APIKEY 
             },
         }).then((res) => {
-            response.send(res)
+            response.send(res.data.trim().replace(/,/g,""))
+        }).catch(error => {
+          console.log(error)
         })
     } 
 }
