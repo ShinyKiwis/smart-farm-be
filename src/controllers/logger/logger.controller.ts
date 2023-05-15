8deb0ade4202ddc031cc64ad5c07d71389fe20cc
lonelyknight2902 <truongthinh2902@gmail.com>
2023-05-16 01:52:54 +0700

    Update: Implement pagination for logger

--- src/controllers/logger/logger.controller.ts
+++ src/controllers/logger/logger.controller.ts
@@ -4,7 +4,7 @@ import Database from 'src/database';
 
 class LoggerController implements Controller {
   public router = express.Router();
-  public path =  '/logger';
+  public path = '/logger';
   log = Database.getInstance().Log;
 
   constructor() {
@@ -12,24 +12,41 @@ class LoggerController implements Controller {
   }
 
   private initializeRoute() {
-    this.router.get(`${this.path}/`, this.getAllLog);
+    this.router.get(`${this.path}`, this.getLog);
     this.router.get(`${this.path}/daily`, this.getDailyLog);
   }
 
-  private getAllLog = async (
+  private getLog = async (
     request: express.Request,
     response: express.Response
   ) => {
-    const logs = await this.log.find();
-    response.send(logs);
+    const page = parseInt(request.query.page as string) || 1;
+    const logsPerPage = 10;
+    const logs = await this.log
+      .find()
+      .sort({ time: -1 })
+      .skip((page - 1) * logsPerPage)
+      .limit(logsPerPage)
+      .exec();
+    const count = await this.log.count();
+    response.send(
+      JSON.stringify({
+        totalPages: Math.ceil(count / logsPerPage),
+        currentPage: page,
+        logs: logs,
+      })
+    );
   };
 
-  private getDailyLog = async (request: express.Request, response: express.Response) => {
+  private getDailyLog = async (
+    request: express.Request,
+    response: express.Response
+  ) => {
     const now = new Date();
     const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
-    const logs = await this.log.find({time: {$gte: today}});
+    const logs = await this.log.find({ time: { $gte: today } });
     response.send(logs);
-  }
+  };
 }
 
 export default LoggerController;
